b287284c6bf6e53415180e0da8047a1a0cc3cee4
yadsashel <ysalhi600@gmail.com>
2024-10-29 03:06:25 -0700

    the task 1 is done

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python3
-"""Task 2: LIFO Caching.
-"""
-from collections import OrderedDict
 
+'''Task 1: FIFO caching
+'''
+
+
+from collections import OrderedDict
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
-    """Represents an object that allows storing and
-    retrieving items from a dictionary with a LIFO
-    removal mechanism when the limit is reached.
-    """
+class FIFOCache(BaseCaching):
+    '''A class `FIFOCache` that inherits from
+       `BaseCaching` and is a caching system.
+    '''
+
     def __init__(self):
-        """Initializes the cache.
-        """
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Adds an item in the cache.
-        """
+        '''assign to the dictionary `self.cache_data` the
+           `item` value for the key `key`
+        '''
+
         if key is None or item is None:
             return
-        if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                last_key, _ = self.cache_data.popitem(True)
-                print("DISCARD:", last_key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {first_key}")
+
         self.cache_data[key] = item
-        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
-        """Retrieves an item by key.
-        """
+        '''return the value in `self.cache_data` linked to `key`
+        '''
         return self.cache_data.get(key, None)
